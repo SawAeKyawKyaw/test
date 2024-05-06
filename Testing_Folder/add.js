7f0a4e88be1b71c9c62e69edb234e8e2ea6574aa
sawaekyawkyaw-nus-study <saw.ae.kyaw.kyaw.sakk@gmail.com>
2024-05-06 14:27:55 +0630

    testing module exports

---
+++ Testing_Folder/add.js
@@ -0,0 +1,3 @@
+module.exports.abb = function add(a,b){
+    return a+b
+}
---
+++ Testing_Folder/app.js
@@ -0,0 +1,12 @@
+var ad = require('./add.js')
+
+// var http = require('http')
+
+// http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     res.end("Welcome back")
+// }).listen(8080)
+
+result = ad.abb(4,5)
+
+console.log("The output is " + result)
